3d0d8411d6b24189b5b8446faad63bf4774efbd8
VedantPol <polvedant7@gmail.com>
2023-08-02 07:40:59 +0530

    added backend

--- php/contact.php
+++ php/contact.php
@@ -43,8 +43,8 @@
 	$email = $_POST['email'];
 	$message = $_POST['message'];
 	$from = $email;
-	$to = 'info@example.com';  // please change this email id
-	$subject = 'Contact Form : Titan - The best downloaded template ever';
+	$to = 'polvedant11@gmail.com';  // please change this email id
+	$subject = 'Contact Form : VisionTechnocraft website';
 
 	$body = "From: $name\n E-Mail: $email\n Message:\n $message";
 
